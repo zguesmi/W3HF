6b1c64b782f87cf824fd9eaefc4d7eb830b33f7d
Zied <26070035+zguesmi@users.noreply.github.com>
2022-10-08 20:07:09 -0500

    Update deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const acceptanceThreshold = 1000;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const W3HF = await ethers.getContractFactory("W3HF");
+  const w3hf = await W3HF.deploy(acceptanceThreshold);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await w3hf.deployed();
 
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`W3HF deployed to ${w3hf.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
