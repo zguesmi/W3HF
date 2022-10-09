68a22cb33f3ce2ddf72a6de9d5828948c9e9a403
Zied <26070035+zguesmi@users.noreply.github.com>
2022-10-08 20:48:15 -0500

    Update hardhat config

--- contracts/W3HF.sol
+++ contracts/W3HF.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
 
 contract W3HF {
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
+import "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
+    networks: {
+        mumbai: {
+            url: process.env.MUMBAI_RPC_URL || "",
+            accounts:
+                process.env.MUMBAI_PRIVATE_KEY !== undefined ? [process.env.MUMBAI_PRIVATE_KEY] : [],
+        },
+    },
 };
 
 export default config;
